5f356ae0402f532140268a1a4212f6787cdc70fa
cemkata <cemkata@users.noreply.github.com>
2024-08-06 17:38:52 +0300

    new starter script

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
 ::start "" "http://127.0.0.1:8080"
+@echo off
 cd /d %~dp0
-%pythonpath%\python.exe quiz.py
+::python-3.9.9-embed-amd64\python.exe quiz.py
+python-3.9.9-embed-amd64\python.exe consoleMenu.py -s
