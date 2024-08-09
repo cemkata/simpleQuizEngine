1ebda91d2fcfd8d3c22571c42f51dd20800fcaae
cemkata <cemkata@users.noreply.github.com>
2024-08-09 13:16:37 +0300

    js fix for restarting the quiz

--- views/static/quiz.js
+++ views/static/quiz.js
@@ -290,18 +290,22 @@ https://www.sitepoint.com/simple-javascript-quiz/
   }
 
   function restartQuiz(){
-	  currentSlide = 0;
-	  clearTimeout(timer);
+      currentSlide = 0;
+      clearTimeout(timer);
       nextButton.classList.add("quzControl");
-	  nextButton.style.display = 'none';
+      nextButton.style.display = 'none';
       submitButton.classList.add("quzControl");
       previousButton.classList.add("quzControl");
       answerButton.classList.add("quzControl");
       document.getElementById("config").classList.remove("quzControl");
-	  document.getElementById("pages").innerHTML = "";
-	  restartButton.classList.add("quzControl");
+      document.getElementById("pages").innerHTML = "";
+      restartButton.classList.add("quzControl");
       // Show empty slide
-      showSlide(currentSlide-1);
+      slidesContainer = document.getElementsByClassName("quiz-container");
+      slidesContainer[0].innerHTML=`<div id="quiz"></div>`;
+      //showSlide(currentSlide);
+      timerTxt.textContent = "No limit";
+      document.getElementById("showTimer").style = "display:none"
   }
 
   function buildQuiz(){
@@ -315,7 +319,13 @@ https://www.sitepoint.com/simple-javascript-quiz/
           countDown = -1;
       }else{
           countDown = parseInt(countDown.value) * 60;
-          if (!isNaN(countDown)) timedCount();
+          if (!isNaN(countDown)) {
+			timedCount();
+		  }else{
+			countDown = document.getElementById("timeInmunites");
+			countDown = parseInt(countDown.value) * 60;
+			timedCount();
+		  }
           document.getElementById("showTimer").style = "display:inline-block;color:#000000;position:absolute;left:65%;top:30%;"
       }
       // Variables
