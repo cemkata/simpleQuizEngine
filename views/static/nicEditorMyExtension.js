77176a0068636b4c9412540aa70ba338a5b425ff
cemkata <cemkata@users.noreply.github.com>
2024-10-11 16:25:48 +0300

    code reorganisation
    Some JS code is moved from the template to the JS file referenced by the template

--- views/static/nicEditorMyExtension.js
+++ views/static/nicEditorMyExtension.js
@@ -509,6 +509,192 @@ function setCheckValues(){
   sum_answer = calc_checksum(document.getElementById("answers_area").innerHTML);
 }
 
+function saveQuestion(content, id, instance) {
+	 var questionID = document.getElementById("questionID").value;
+	 var courseID = document.getElementById("courseID").value;
+	 var quizID = document.getElementById("quizID").value;
+	
+	 let tmpHolder = document.getElementsByClassName(' nicEdit-main ');
+
+	 //var questionTxt = document.getElementById("area_question").value; //do not use the area, but the div and the inner text
+	 var questionTxt = tmpHolder[0].innerHTML;
+	 //var explnTxt = document.getElementById("area_explanation").value; //do not use the area, but the div and the inner text
+	 var explnTxt = tmpHolder[1].innerHTML;
+	 var referenceLink = document.getElementById("referenceLink").value;
+	
+	if(document.getElementById("select_answers") != null){ //Drag-drop answers
+		selc_ans = document.getElementById("select_answers");
+		posb_ans = document.getElementById("correct_answers");
+		var answers_html = selc_ans.getElementsByClassName("showinline");
+		answers = [];
+		answerCount = [];
+		for(let i = 0; i < answers_html.length; i++){
+			answers.push(answers_html[i].firstChild.value)
+			answerCount.push(answers_html[i].childNodes[2].value)
+		}
+		var answers_html = posb_ans.getElementsByClassName("showinline");
+		correctAnswer = [];
+		groups = [];
+		for(let i = 0; i < answers_html.length; i++){
+			correctAnswer.push(answers_html[i].firstChild.value)
+			groups.push(answers_html[i].childNodes[2].value)
+		}
+		correctAnswer = JSON.stringify(correctAnswer)
+		
+		if(!questionTxt.includes("$?__")){
+			for(let i = 0; i < answers_html.length; i++){
+				questionTxt+="<div>&nbsp;$?__</div>"
+			}
+		}else{
+			var count = (questionTxt.match(/\$\?__/g) || []).length;
+			if(count != answers_html.length){
+			   alert("Drop darget count diffrent than correct answers!")
+			   errorDiv = document.getElementById("error_no_answer");
+			   errorDiv.style.setProperty('display','block','')
+			   return;
+			}
+		}
+	}else{ //free text
+		 var freetext = document.getElementById("freeTextAns");
+		 if(freetext == null){
+		   //Not free text question
+		   var answers_html = document.getElementsByClassName("showinline");
+		   var correctAnswer = ""
+		   var answers = {};
+		   // -1 becasue the reference link adds one more line
+		   for(let i = 0; i < answers_html.length - 1; i++){
+			 var tmp_ans = answers_html[i].getElementsByTagName("input");
+			 if(tmp_ans[0].checked){
+			   //correctAnswer+=i;
+			   correctAnswer+=String.fromCharCode(65 + i);
+			 }
+			 //answers[i] = tmp_ans[1].value
+			 answers[String.fromCharCode(65 + i)] = tmp_ans[1].value
+		   }
+		 }else{
+		   var answers = {};
+		   var correctAnswer = document.getElementById("freeTextAns").value;
+		 }
+	 }
+
+	 var fd = new FormData(); // https://hacks.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
+	 fd.append("courseID", courseID);
+	 fd.append("quizID", quizID);
+	 fd.append("questionID", questionID);
+	 fd.append("questionTxt",  questionTxt);
+	 fd.append("explnTxt", explnTxt);
+	 fd.append("referenceLink", referenceLink);
+	 fd.append("answers", JSON.stringify(answers));
+	 if(typeof groups !== 'undefined'){
+		for(let j = 0; j < groups.length; j++){
+			if(groups[j] == j){continue}
+			fd.append("answers_grp", JSON.stringify(groups));
+			break;
+		}
+	 }
+	 if(typeof answerCount !== 'undefined'){
+		for(let j = 0; j < answerCount.length; j++){
+			if(answerCount[j] == "1"){continue}
+			fd.append("answers_cnt", JSON.stringify(answerCount));
+			break;
+		}
+	 }
+	 fd.append("correctAnswer", correctAnswer);
+	 if(correctAnswer.length <= 0){ //add validation
+	   alert("Please fill the correct answer!")
+	   errorDiv = document.getElementById("error_no_answer");
+	   errorDiv.style.setProperty('display','block','')
+	   return;
+	 }
+
+	 var xhr = new XMLHttpRequest();
+	 xhr.open("POST", "./saveQuestion");
+
+	 xhr.onload = function() {
+			 alert("Status: " + xhr.responseText);
+			 if(xhr.status == 200){
+			   setCheckValues();
+			 }
+			 errorDiv = document.getElementById("error_no_answer");
+			 errorDiv.style.display='none'
+	 };
+	 xhr.onerror = xhr.onload;
+	 //xhr.setRequestHeader('Authorization', 'Client-ID c37fc05199a05b7');
+	 xhr.send(fd);
+  }
+
+  function clearPage(){
+	if(isModified()) {
+		if(!confirm("The question is not saved.\nDo you want to clear the page?")) {
+			 event.stopPropagation();
+			 event.preventDefault()
+			 return;
+		}
+	}
+	var options = document.getElementById("questionType").options;
+	options[0].selected = true;
+	let tmpHolder = document.getElementsByClassName(' nicEdit-main ');
+	//var questionTxt = document.getElementById("area_question").value; //do not use the area, but the div and the inner text
+	tmpHolder[0].innerHTML = "";
+	//var explnTxt = document.getElementById("area_explanation").value; //do not use the area, but the div and the inner text
+	tmpHolder[1].innerHTML = "";
+	var ansArea = document.getElementById("answers_area");
+	newHtml = `<input type="text" id="freeTextAns" style = "width: 100%;" value="">`;
+	ansArea.innerHTML = newHtml;
+	document.getElementById("noQuestion").value = 0;
+	document.getElementById("referenceLink").value = ""
+	errorDiv = document.getElementById("error_no_answer");
+	errorDiv.style.display='none'
+	setCheckValues();
+  }
+
+  function nicEdit_Scroll_Patch(){
+	let tmpHolder = document.getElementsByClassName(' nicEdit-main ')
+	var body = document.body,
+		html = document.documentElement;
+	var width = tmpHolder[0].clientWidth;
+
+	var height = Math.max( body.scrollHeight, body.offsetHeight,
+						   html.clientHeight, html.scrollHeight, html.offsetHeight ) / 4;
+	for (let i = 0; i < tmpHolder.length; i++){
+		tmpHolder[i].style.setProperty('max-height',height,'');
+		tmpHolder[i].style.setProperty('overflow-y','scroll','');
+		//tmpHolder[i].style.setProperty('max-width',width,'');
+		//tmpHolder[i].style.setProperty('overflow-x','scroll','');
+	}
+  }
+
+  function nicEdit_Padding_Patch(){
+	let tmpHolder = document.getElementsByClassName(' nicEdit-main ')
+	for (let i = 0; i < tmpHolder.length; i++){
+		tmpHolder[i].style.setProperty('padding-left','5px','');
+	}
+  }
+  
+  var selectedColor = '#fff';
+  function nicEdit_BackgroundColor_Patch(){
+	let tmpHolder = document.getElementsByClassName('editor_holder')
+	for (let i = 0; i < tmpHolder.length; i++){
+		tmpHolder[i].children[1].style.setProperty('background-color',selectedColor,'');
+		//tmpHolder[i].children[1].style.setProperty('background-color','#fff','');
+	}
+  }
+  
+  function initEditors(){
+	  new nicEditor({fullPanel : true, /*uploadImgURI : '/be/nicUploadImg',*/
+	  onSave : saveQuestion,
+	  iconsPath : '/static/nic/new_nicEditorIcons.png',
+	  tableURL : '/be/nicShowFiles'}).panelInstance('area_question');
+
+	  new nicEditor({fullPanel : true, /*uploadImgURI : '/be/nicUploadImg',*/
+	  onSave : saveQuestion,
+	  iconsPath : '/static/nic/new_nicEditorIcons.png',
+	  tableURL : '/be/nicShowFiles'}).panelInstance('area_explanation');
+	  nicEdit_Padding_Patch();
+	  nicEdit_Scroll_Patch();
+	  //nicEdit_BackgroundColor_Patch();
+ }
+
 function calc_checksum(in_str){
 	return calculateCRC(in_str);
 }
@@ -534,17 +720,10 @@ function calculateCRC(data) {
     return crc ^ 0xFFFFFFFF;
 }
 
-/*
+
 // used from https://stackoverflow.com/questions/14733374/how-to-generate-an-md5-hash-from-a-string-in-javascript-node-js
-var MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
-
-var result = MD5(value);
- 
-document.body.innerHTML = 'hash - normal words: ' + result;
-
-//--* NON-ENGLISH words **-//
-value = 'מבחן'
-
-// unescape() can be deprecated for the new browser versions
-result = MD5(unescape(encodeURIComponent(value)));
-*/
+function calculateMD5(data){
+    var MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
+    var result = MD5(data);
+    return result;
+}
